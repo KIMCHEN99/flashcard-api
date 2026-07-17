3b72fcbe68acea3cdf0bfb5201087dd2c3db9262
KIMCHEN99 <poketmon624@gmail.com>
2026-07-17 20:19:37 +0900

    fix CORS error

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # CORS 모듈 불러오기
 from app.routes import router
 
 app = FastAPI()
 
-# 라우터 연결
-app.include_router(router)
+# --- CORS(보안 해제) 설정 추가 ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 주소(Netlify 포함)에서 오는 요청 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT 등 모든 방식 허용
+    allow_headers=["*"],
+)
+# ------------------------------
 
-@app.get("/")
-def read_root():
-    return {"message": "Flashcard API is running!"}
+app.include_router(router)
